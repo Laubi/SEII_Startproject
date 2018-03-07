6352d677bee091100581c0ad44de6461d235a7cc
David Laubreiter <david.laubreiter@trinitec.at>
2018-03-07 10:33:37 +0100

    Initialize Android project

---
+++ app/src/main/java/at/laubi/palindromchecker/MainActivity.java
@@ -0,0 +1,11 @@
+package at.laubi.palindromchecker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+}
