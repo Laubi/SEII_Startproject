72afbcecc38b9035deb4962917d4a0a3729d1e00
David Laubreiter <david.laubreiter@trinitec.at>
2018-03-07 10:34:54 +0100

    Implement project

--- app/src/main/java/at/laubi/palindromchecker/MainActivity.java
+++ app/src/main/java/at/laubi/palindromchecker/MainActivity.java
@@ -7,5 +7,48 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView textInput;
+    private TextView messages;
+    @SuppressWarnings("FieldCanBeLocal")
+    private Button btnSubmit;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.textInput = this.findViewById(R.id.tvText);
+        this.btnSubmit = this.findViewById(R.id.btnCheckString);
+        this.messages = this.findViewById(R.id.tvMessages);
+
+        this.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPalindrom();
+            }
+        });
+    }
+
+    public static boolean isPalindrome(String str)	{
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    private void checkPalindrom() {
+        String input = this.textInput.getText().toString();
+
+        if(input.length() < 5) {
+            setMessage("Input is to short");
+        }else {
+            if(isPalindrome(input)) {
+                setMessage("Yes, the input is a palindrom");
+            }else {
+                setMessage("No, the input is not a palindrom");
+            }
+        }
+    }
+
+    private void setMessage(String str) {
+        this.messages.setText(str);
+    }
 }
